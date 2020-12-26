77c4f807072784dc9ca181468fe63f4af04ced21
Nikola <n@bobetic.com>
2020-12-26 10:12:59 +0100

    Create install_zerotier.sh

---
+++ install_zerotier.sh
@@ -0,0 +1,2 @@
+curl -s https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg | gpg --import
+curl -s https://install.zerotier.com/ | gpg --output - >/tmp/zt-install.sh && bash /tmp/zt-install.sh
