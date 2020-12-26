bacffc66039c580cf897dea940afae432a2334d9
Nikola <n@bobetic.com>
2020-12-26 10:10:47 +0100

    Update install_before_hassio.sh

--- install_before_hassio.sh
+++ install_before_hassio.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-apt-get install -y software-properties-common apparmor-utils apt-transport-https avahi-daemon ca-certificates curl dbus jq socat
+apt-get install -y software-properties-common apparmor-utils apt-transport-https avahi-daemon ca-certificates curl dbus jq network-manager socat
+systemctl disable ModemManager
+systemctl stop ModemManager
