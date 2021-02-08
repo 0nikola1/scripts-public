fbec66903e15b0d97c33b8d4846fbc889e8276e2
Nikola <n@bobetic.com>
2021-02-08 16:22:53 +0100

    Update install_ha_core_configurator.sh

--- addons/install_ha_core_configurator.sh
+++ addons/install_ha_core_configurator.sh
@@ -1,3 +1,7 @@
+
+echo "Starting core configurator install"
 ha addons install core_configurator
+echo "Sleeping for 5 seconds…"
 sleep 5
 ha addons start core_configurator
+echo "Core configurator started"
